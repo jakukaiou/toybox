bf5f6df2bc2fcd06437e9dd0e1e737cde95ccb4c
tekkyuu <byabyabya.dx@gmail.com>
2017-05-22 00:38:42 +0900

    use full-livereload-example

---
+++ client/page1/index.js
@@ -0,0 +1,13 @@
+require("./index.scss");
+require("../common/common.js");
+require("./index.html");
+
+console.log("client/page1/index.js");
+
+var drawEl = document.getElementById("draw");
+drawEl.innerHTML = "peko";
+
+// Uncomment these to enable hot module reload for this entry.
+// if (module.hot) {
+//   module.hot.accept();
+// }
---
+++ webpack.production.config.js
@@ -0,0 +1,42 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path = require('path');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+
+var productionConfig = [{
+    entry: {
+        page1: './client/page1',
+        page2: './client/page2'
+    },
+    output: {
+        filename: './[name]/bundle.js',
+        path: path.resolve(__dirname, './public'),
+        publicPath: '/'
+    },
+    module: {
+        rules: [{
+            test: /\.(png|jpg)$/,
+            use: 'url-loader?limit=8192&context=client&name=[path][name].[ext]'
+        }, {
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: ['css-loader', 'resolve-url-loader', 'sass-loader?sourceMap']
+            })
+        }, {
+            test: /\.html$/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: 'file-loader?context=client&name=[path][name].[ext]'
+            })
+        }]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['public']),
+        new ExtractTextPlugin({
+            filename: './[name]/index.css',
+            allChunks: true
+        })
+    ]
+}];
+
+module.exports = productionConfig;
